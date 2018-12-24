80edeb16b425506cca59a8787aab21c2a681e6a8
Kenneth Evans <ken@kenevans.net>
2018-12-23 19:24:27 -0500

    Implemented password to get 5000 requests per hr.  Seems to be working.

--- src/misc/ParseGitHubRepositories.java
+++ src/misc/ParseGitHubRepositories.java
@@ -1,7 +1,18 @@
 package misc;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.io.FileReader;
 import java.util.Date;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 
 import com.google.gson.Gson;
 
@@ -58,17 +69,28 @@ class Rate
 
 public class ParseGitHubRepositories
 {
-    private static String DEFAULT_IN_FILE = "C:/Scratch/AAA/github-repos-2018-12-22.json";
-    private static String REPRO_COMMAND = "curl https://api.github.com/users/kennethevans/repos?per_page=1000";
-    private static String RELEASE_COMMAND_PRE = "curl https://api.github.com/repos/kennethevans/";
-    private static String RELEASE_CMDOMMAND_POST = "/releases?per_page=1000";
-    private static String RATE_LIMIT_COMMAND = "curl https://api.github.com/rate_limit";
-    private static String inFile = DEFAULT_IN_FILE;
+    private static final String DEFAULT_USER = "KennethEvans";
+    private static final String DEFAULT_IN_FILE = "C:/Scratch/AAA/github-repos-2018-12-22.json";
+    private static final String REPRO_COMMAND = "https://api.github.com/users/"
+        + DEFAULT_USER + "/repos?per_page=1000";
+    private static final String RELEASE_COMMAND_PRE = "https://api.github.com/repos/"
+        + DEFAULT_USER + "/";
+    private static final String RELEASE_COMMAND_POST = "/releases?per_page=1000";
+    private static final String RATE_LIMIT_COMMAND = "https://api.github.com/rate_limit";
+    private static final String inFile = DEFAULT_IN_FILE;
+
+    private static final boolean USE_PASSWORD = true;
+    private static final boolean GET_RELEASES = USE_PASSWORD ? true : false;
+
+    private static char[] userName = DEFAULT_USER.toCharArray();
+    private static char[] password = "".toCharArray();
+    private static boolean havePassword = false;
 
     public static void listRepositories() {
         String json = null;
+        String cmd = generateCurlCommand(REPRO_COMMAND);
         try {
-            json = execCmd(REPRO_COMMAND);
+            json = execCmd(cmd);
         } catch(Exception ex) {
             ex.printStackTrace();
             return;
@@ -94,20 +116,23 @@ public class ParseGitHubRepositories
                 System.out.println("    updated_at=" + repro.updated_at);
                 System.out.println("    pushed_at=" + repro.pushed_at);
                 System.out.println("    size=" + repro.size);
-                releases = getRelease(repro.name);
-                if(releases != null || releases.length == 0) {
-                    System.out.println("    No releases");
-                } else {
-                    System.out.println("    Releases");
-                    for(Release release : releases) {
-                        System.out.println("      " + release.name);
-                        System.out.println(
-                            "            tag_name=" + release.tag_name);
-                        System.out.println("          body=" + release.body);
-                        System.out.println(
-                            "           created_at=" + release.created_at);
-                        System.out.println(
-                            "           published_at=" + release.published_at);
+                if(GET_RELEASES) {
+                    releases = getRelease(repro.name);
+                    if(releases == null || releases.length == 0) {
+                        System.out.println("    No releases");
+                    } else {
+                        System.out.println("    Releases");
+                        for(Release release : releases) {
+                            System.out.println("      " + release.name);
+                            System.out.println(
+                                "          tag_name=" + release.tag_name);
+                            System.out
+                                .println("          body=" + release.body);
+                            System.out.println(
+                                "           created_at=" + release.created_at);
+                            System.out.println("           published_at="
+                                + release.published_at);
+                        }
                     }
                 }
             }
@@ -117,9 +142,54 @@ public class ParseGitHubRepositories
         }
     }
 
+    public static String generateCurlCommand(String arg) {
+        if(USE_PASSWORD) {
+            if(!havePassword) {
+                if(!getPassword()) {
+                    return "curl " + arg;
+                } else {
+                    return "curl -u \"" + String.valueOf(userName) + ":"
+                        + String.valueOf(password) + "\" " + arg;
+                }
+            } else {
+                return "curl -u \"" + String.valueOf(userName) + ":"
+                    + String.valueOf(password) + "\" " + arg;
+            }
+        }
+        return "curl " + arg;
+    }
+
+    public static boolean getPassword() {
+        JPanel panel = new JPanel(new BorderLayout(5, 5));
+
+        JPanel label = new JPanel(new GridLayout(0, 1, 2, 2));
+        label.add(new JLabel("Username", SwingConstants.RIGHT));
+        label.add(new JLabel("Password", SwingConstants.RIGHT));
+        panel.add(label, BorderLayout.WEST);
+
+        JPanel controls = new JPanel(new GridLayout(0, 1, 2, 2));
+        JTextField usernameField = new JTextField();
+        usernameField.setText(DEFAULT_USER);
+        controls.add(usernameField);
+        JPasswordField passwordField = new JPasswordField();
+        controls.add(passwordField);
+        panel.add(controls, BorderLayout.CENTER);
+
+        int option = JOptionPane.showConfirmDialog(null, panel, "Login",
+            JOptionPane.OK_CANCEL_OPTION);
+        if(option == JOptionPane.OK_OPTION) {
+            password = passwordField.getPassword();
+            userName = usernameField.getText().toCharArray();
+            havePassword = true;
+            return true;
+        }
+        return false;
+    }
+
     public static Release[] getRelease(String repoName) {
         Gson gson = new Gson();
-        String cmd = RELEASE_COMMAND_PRE + repoName + RELEASE_CMDOMMAND_POST;
+        String cmd = generateCurlCommand(
+            RELEASE_COMMAND_PRE + repoName + RELEASE_COMMAND_POST);
         String json;
         try {
             json = execCmd(cmd);
@@ -146,6 +216,7 @@ public class ParseGitHubRepositories
     }
 
     public static void readFile(String fileName) {
+        System.out.println(inFile);
         try {
             Gson gson = new Gson();
             Repro[] results = gson.fromJson(new FileReader(DEFAULT_IN_FILE),
@@ -201,7 +272,7 @@ public class ParseGitHubRepositories
 
     public static boolean checkMessage(String json, String caller) {
         Message message = getMessage(json);
-        if(message != null) {
+        if(message != null && message.message != null) {
             System.out.println(caller + " received:\n  " + message.message);
             if(message.message.contains("API rate limit exceeded")) {
                 listRateLimit();
@@ -213,7 +284,7 @@ public class ParseGitHubRepositories
 
     public static void listRateLimit() {
         Gson gson = new Gson();
-        String cmd = RATE_LIMIT_COMMAND;
+        String cmd = generateCurlCommand(RATE_LIMIT_COMMAND);
         String json;
         try {
             json = execCmd(cmd);
@@ -222,11 +293,7 @@ public class ParseGitHubRepositories
             return;
         }
         RateLimit rateLimit = gson.fromJson(json, RateLimit.class);
-        Message message = getMessage(json);
-        if(message != null && message.message != null) {
-            System.out.println("listRateLimit received:\n  " + message.message);
-            return;
-        }
+        if(!checkMessage(json, "listRateLimit")) return;
         System.out.println("Rate Limit:");
         System.out.println("    limit=" + rateLimit.rate.limit);
         System.out.println("    remaining=" + rateLimit.rate.remaining);
@@ -239,12 +306,21 @@ public class ParseGitHubRepositories
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println(inFile);
+        try {
+            // Set window decorations
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            // Set the native look and feel
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch(Throwable t) {
+            t.printStackTrace();
+        }
+
         System.out.println(new Date());
         // listRateLimit(); // Use to check when rate will reset
         listRepositories();
         // listRelease("VS-ArtPad");
         // listRelease("VS-Fractal");
+
         System.out.println();
         System.out.println("All Done");
     }
