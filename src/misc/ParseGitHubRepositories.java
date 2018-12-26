489cf166e37ec5ad1fc2c1e1c26800e764f56ef2
Kenneth Evans <ken@kenevans.net>
2018-12-25 20:37:52 -0500

    Rewrote logic for generating commands.  Added getReadme.

--- src/misc/ParseGitHubRepositories.java
+++ src/misc/ParseGitHubRepositories.java
@@ -55,6 +55,11 @@ class Message
     String message;
 }
 
+class Readme
+{
+    String path;
+}
+
 class RateLimit
 {
     Rate rate;
@@ -69,26 +74,27 @@ class Rate
 
 public class ParseGitHubRepositories
 {
-    private static final String DEFAULT_USER = "KennethEvans";
+    private static final String DEFAULT_OWNER = "KennethEvans";
     private static final String DEFAULT_IN_FILE = "C:/Scratch/AAA/github-repos-2018-12-22.json";
-    private static final String REPRO_COMMAND = "https://api.github.com/users/"
-        + DEFAULT_USER + "/repos?per_page=1000";
-    private static final String RELEASE_COMMAND_PRE = "https://api.github.com/repos/"
-        + DEFAULT_USER + "/";
-    private static final String RELEASE_COMMAND_POST = "/releases?per_page=1000";
-    private static final String RATE_LIMIT_COMMAND = "https://api.github.com/rate_limit";
     private static final String inFile = DEFAULT_IN_FILE;
 
     private static final boolean USE_PASSWORD = true;
     private static final boolean GET_RELEASES = USE_PASSWORD ? true : false;
+    private static final boolean GET_README = USE_PASSWORD ? true : false;
+    private static String owner = DEFAULT_OWNER;
 
-    private static char[] userName = DEFAULT_USER.toCharArray();
+    /**
+     * Note that user is the different from owner, even though they may
+     * typically be the same. User is used for logging in. Owner is the owner of
+     * the GitHUb account.
+     */
+    private static char[] userName = DEFAULT_OWNER.toCharArray();
     private static char[] password = "".toCharArray();
     private static boolean havePassword = false;
 
     public static void listRepositories() {
         String json = null;
-        String cmd = generateCurlCommand(REPRO_COMMAND);
+        String cmd = generateCurlCommand(generateReproCommand());
         try {
             json = execCmd(cmd);
         } catch(Exception ex) {
@@ -99,7 +105,9 @@ public class ParseGitHubRepositories
         try {
             Gson gson = new Gson();
             Repro[] results = gson.fromJson(json, Repro[].class);
-            Release[] releases;
+            Release[] releases = null;
+            ;
+            Readme readme = null;
             for(Repro repro : results) {
                 System.out.println(repro.full_name);
                 System.out.println("    name=" + repro.name);
@@ -116,6 +124,14 @@ public class ParseGitHubRepositories
                 System.out.println("    updated_at=" + repro.updated_at);
                 System.out.println("    pushed_at=" + repro.pushed_at);
                 System.out.println("    size=" + repro.size);
+                if(GET_README) {
+                    readme = getReadme(repro.name);
+                    if(readme == null) {
+                        System.out.println("    No readme");
+                    } else {
+                        System.out.println("    readme=" + readme.path);
+                    }
+                }
                 if(GET_RELEASES) {
                     releases = getRelease(repro.name);
                     if(releases == null || releases.length == 0) {
@@ -140,6 +156,94 @@ public class ParseGitHubRepositories
         } catch(Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    public static void listRelease(String repoName) {
+        Release[] releases = getRelease(repoName);
+        if(releases == null) {
+            return;
+        }
+        for(Release release : releases) {
+            System.out.println("name=" + release.name);
+            System.out.println("    tag_name=" + release.tag_name);
+            System.out.println("    body=" + release.body);
+            System.out.println("    created_at=" + release.created_at);
+            System.out.println("    published_at=" + release.published_at);
+        }
+    }
+
+    public static Release[] getRelease(String repoName) {
+        Gson gson = new Gson();
+        String cmd = generateCurlCommand(generateReleaseCommand(repoName));
+        String json;
+        try {
+            json = execCmd(cmd);
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        if(!checkMessage(json, "getRelease")) return null;
+        return gson.fromJson(json, Release[].class);
+    }
+
+    public static Readme getReadme(String repoName) {
+        Gson gson = new Gson();
+        String cmd = generateCurlCommand(generateReadmeCommand(repoName));
+        String json;
+        try {
+            json = execCmd(cmd);
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        // Check for not found first
+        Message message = getMessage(json);
+        if(message != null && message.message != null) {
+            if(message.message.toLowerCase().contains("not found")) {
+                return null;
+            }
+        }
+        // Check for error
+        if(!checkMessage(json, "getRelease")) return null;
+        return gson.fromJson(json, Readme.class);
+    }
+
+    public static void listRateLimit() {
+        Gson gson = new Gson();
+        String cmd = generateCurlCommand(generateRateLimitCommand());
+        String json;
+        try {
+            json = execCmd(cmd);
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            return;
+        }
+        RateLimit rateLimit = gson.fromJson(json, RateLimit.class);
+        if(!checkMessage(json, "listRateLimit")) return;
+        System.out.println("Rate Limit:");
+        System.out.println("    limit=" + rateLimit.rate.limit);
+        System.out.println("    remaining=" + rateLimit.rate.remaining);
+        long time = rateLimit.rate.reset;
+        Date remaining = new Date(time * 1000);
+        System.out.println("    reset=" + remaining);
+    }
+
+    public static String generateReproCommand() {
+        return "https://api.github.com/users/" + owner + "/repos?per_page=1000";
+    }
+
+    public static String generateReleaseCommand(String repoName) {
+        return "https://api.github.com/repos/" + owner + "/" + repoName
+            + "/releases?per_page=1000";
+    }
+
+    public static String generateReadmeCommand(String repoName) {
+        return "https://api.github.com/repos/" + owner + "/" + repoName
+            + "/readme?per_page=1000";
+    }
+
+    public static String generateRateLimitCommand() {
+        return "https://api.github.com/rate_limit";
     }
 
     public static String generateCurlCommand(String arg) {
@@ -169,7 +273,7 @@ public class ParseGitHubRepositories
 
         JPanel controls = new JPanel(new GridLayout(0, 1, 2, 2));
         JTextField usernameField = new JTextField();
-        usernameField.setText(DEFAULT_USER);
+        usernameField.setText(DEFAULT_OWNER);
         controls.add(usernameField);
         JPasswordField passwordField = new JPasswordField();
         controls.add(passwordField);
@@ -184,64 +288,6 @@ public class ParseGitHubRepositories
             return true;
         }
         return false;
-    }
-
-    public static Release[] getRelease(String repoName) {
-        Gson gson = new Gson();
-        String cmd = generateCurlCommand(
-            RELEASE_COMMAND_PRE + repoName + RELEASE_COMMAND_POST);
-        String json;
-        try {
-            json = execCmd(cmd);
-        } catch(Exception ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        if(!checkMessage(json, "getRelease")) return null;
-        return gson.fromJson(json, Release[].class);
-    }
-
-    public static void listRelease(String repoName) {
-        Release[] releases = getRelease(repoName);
-        if(releases == null) {
-            return;
-        }
-        for(Release release : releases) {
-            System.out.println("name=" + release.name);
-            System.out.println("    tag_name=" + release.tag_name);
-            System.out.println("    body=" + release.body);
-            System.out.println("    created_at=" + release.created_at);
-            System.out.println("    published_at=" + release.published_at);
-        }
-    }
-
-    public static void readFile(String fileName) {
-        System.out.println(inFile);
-        try {
-            Gson gson = new Gson();
-            Repro[] results = gson.fromJson(new FileReader(DEFAULT_IN_FILE),
-                Repro[].class);
-            for(Repro repro : results) {
-                System.out.println(repro.full_name);
-                System.out.println("    name=" + repro.name);
-                System.out.println("    description=" + repro.description);
-                System.out.println("    language=" + repro.language);
-                // Always true ?
-                System.out.println("    has_downloads=" + repro.has_downloads);
-                if(repro.license != null) {
-                    System.out.println("    license=" + repro.license.name);
-                } else {
-                    System.out.println("    license=");
-                }
-                System.out.println("    created_at=" + repro.created_at);
-                System.out.println("    updated_at=" + repro.updated_at);
-                System.out.println("    pushed_at=" + repro.pushed_at);
-                System.out.println("    size=" + repro.size);
-            }
-
-        } catch(Exception ex) {
-            ex.printStackTrace();
-        }
     }
 
     public static String execCmd(String cmd) throws java.io.IOException {
@@ -282,24 +328,39 @@ public class ParseGitHubRepositories
         return true;
     }
 
-    public static void listRateLimit() {
-        Gson gson = new Gson();
-        String cmd = generateCurlCommand(RATE_LIMIT_COMMAND);
-        String json;
+    /**
+     * Read from a previously-generated file rather than using cURL to access
+     * the GitHub API>
+     * 
+     * @param fileName File with JSON from a repositories request.
+     */
+    public static void readFile(String fileName) {
+        System.out.println(inFile);
         try {
-            json = execCmd(cmd);
+            Gson gson = new Gson();
+            Repro[] results = gson.fromJson(new FileReader(DEFAULT_IN_FILE),
+                Repro[].class);
+            for(Repro repro : results) {
+                System.out.println(repro.full_name);
+                System.out.println("    name=" + repro.name);
+                System.out.println("    description=" + repro.description);
+                System.out.println("    language=" + repro.language);
+                // Always true ?
+                System.out.println("    has_downloads=" + repro.has_downloads);
+                if(repro.license != null) {
+                    System.out.println("    license=" + repro.license.name);
+                } else {
+                    System.out.println("    license=");
+                }
+                System.out.println("    created_at=" + repro.created_at);
+                System.out.println("    updated_at=" + repro.updated_at);
+                System.out.println("    pushed_at=" + repro.pushed_at);
+                System.out.println("    size=" + repro.size);
+            }
+
         } catch(Exception ex) {
             ex.printStackTrace();
-            return;
         }
-        RateLimit rateLimit = gson.fromJson(json, RateLimit.class);
-        if(!checkMessage(json, "listRateLimit")) return;
-        System.out.println("Rate Limit:");
-        System.out.println("    limit=" + rateLimit.rate.limit);
-        System.out.println("    remaining=" + rateLimit.rate.remaining);
-        long time = rateLimit.rate.reset;
-        Date remaining = new Date(time * 1000);
-        System.out.println("    reset=" + remaining);
     }
 
     /**
