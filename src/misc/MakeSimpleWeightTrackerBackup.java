e40f948674432fbab9d1535de51e60fca272595f
Kenneth Evans <ken@kenevans.net>
2019-12-29 19:23:50 -0500

    MakeSimpleWeightTrackerBackup: Changed from Collections to List.

--- src/misc/MakeSimpleWeightTrackerBackup.java
+++ src/misc/MakeSimpleWeightTrackerBackup.java
@@ -1,14 +1,12 @@
 package misc;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import javax.swing.JFrame;
